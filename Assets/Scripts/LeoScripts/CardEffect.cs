df9566fd162a64cf36a32649f3360b6e0403689e
Bailek <55186594+Bailek@users.noreply.github.com>
2021-09-08 17:50:47 +0200

    wip Card effect

---
+++ Assets/Scripts/LeoScripts/CardEffect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardEffect : MonoBehaviour
+{
+    public GameObject 
+    public void Effect()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/LeoScripts/Drop.cs
+++ Assets/Scripts/LeoScripts/Drop.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class Drop : MonoBehaviour, IDropHandler
 {
-    public Drag.Slot typeOfCard = Drag.Slot.TOURET;
-    public Transform newParent = null;
-    
-    
     public void OnDrop(PointerEventData eventData)
     {
         CardDisplay c = eventData.pointerDrag.GetComponent<CardDisplay>();
